bfceec33afd675d6729ece32fddc30ca1927d261
Maksim Minakov <maksim.minakov.83@mail.ru>
2026-02-19 16:53:55 +0300

    Initial Commit

---
+++ module3lesson7RepresentableHomework/module3lesson7RepresentableHomeworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  module3lesson7RepresentableHomeworkApp.swift
+//  module3lesson7RepresentableHomework
+//
+//  Created by Максим Минаков on 19.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct module3lesson7RepresentableHomeworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
